a099cf6f7749ed5ac5f1fee4d9a8593247b99ff9
Robin Van den Broeck <robin.broeck@gmail.com>
2020-03-29 15:06:18 +0200

    Save jwt + refresh token

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,10 +4,13 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== "production",
   state: {
     user: {
       loggedIn: false,
-      data: null
+      data: null,
+      jwt: null,
+      refreshToken: null
     }
   },
   getters: {
@@ -21,16 +24,25 @@ export default new Vuex.Store({
     },
     SET_USER(state, data) {
       state.user.data = data;
+    },
+    SET_JWT(state, data) {
+      state.user.jwt = data;
+    },
+    SET_REFERSH_TOKEN(state, data) {
+      state.refreshToken = data;
     }
   },
   actions: {
-    fetchUser({ commit }, user) {
+    async fetchUser({ commit }, user) {
       commit("SET_LOGGED_IN", user !== null);
       if (user) {
+        const idToken = await user.getIdToken();
         commit("SET_USER", {
           displayName: user.displayName,
           email: user.email
         });
+        commit("SET_JWT", idToken);
+        commit("SET_REFERSH_TOKEN", user.refreshToken);
       } else {
         commit("SET_USER", null);
       }
